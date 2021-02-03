96ddb192fa830d9e00f88289577cc968dbf905dd
Ibidapo-chad <Ibidapo.chad@gmail.com>
2021-02-03 20:54:13 +0000

    scripts and program files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return - Ends program
+ */
+int main(void)
+{
+  int ch 
+  int putc()
+  return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+**This is aC programming test project**
