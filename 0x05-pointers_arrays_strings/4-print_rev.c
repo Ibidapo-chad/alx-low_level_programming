5d616beee6e2f3d9459ed73f7a3e5cde83ad4977
Ibidapo-chad <ibidapo.chad@gmail.com>
2021-02-17 20:20:49 +0000

    print rev modified

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,9 +2,8 @@
 
 void print_rev(char *s)
 {
-  int i, n;
-  n = _strlen(s);
-  for (i = n; *(s - i) != '\0'; i--)
+  int i;
+    for (i = -1; *(s + i) != '\0'; i--)
     _putchar(*(s - i));
   _putchar('\n');
 }
