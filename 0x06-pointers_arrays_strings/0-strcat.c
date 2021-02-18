2e6733b0f4e000bebd1849718cc4e2d8894f2176
Ibidapo-chad <ibidapo.chad@gmail.com>
2021-02-18 13:42:23 +0000

    0x06-folders, files, README and .h file created

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * Program defines concatenation 
+ * of two strings
+ */
+char *_strcat(char *dest, char *src)
+{
+  
+}
+
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+
+/* concatenation of two strings with null termination */
+char *_strcat(char *dest, char *src);
+
+/* concatenation of two strings without null termination 
+ * where n is number of bytes
+ */
+char *_strncat(char *dest, char *src, int n);
+
+/* copies a string */
+char *_strncpy(char *dest, char *src, int n);
+
+/* It compares two strings */
+int _strcmp(char *s1, char *s2);
+
+/* Reverses the content of an array */
+void reverse_array(int *a, int n);
+
+/* Converts lowercase characters to uppercase characters */
+char *string_toupper(char *);
+
+/* Capitalizes all words of a string */
+char *cap_string(char *);
+
+/* Encodes a string into 1337 */
+char *leet(char *);
+
+/* Encodes a string into rot13 */
+char *rot13(char *);
