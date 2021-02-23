ec0aa93211aaf748b5c79b4b0f0d79f350f0ea0e
Ibidapo-chad <ibidapo.chad@gmail.com>
2021-02-23 20:26:51 +0000

    diagsums algorithm created

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,9 +7,17 @@
 
 void print_diagsums(int *a, int size)
 {
-  int i, j;
-  for ()
+  int i, sum_1, sum_2;
+  sum_1 = 0;
+  for (i = 0; i < size; i++)
   {
-    
+    sum_1 += *(a + ((size * i) + i));
   }
+  sum_2 = 0;
+  for (i = 0; i < size; i++)
+    {
+      sum_2 += *(a + ((size * i) - i));
+    }
+  _putchar(sum_1);
+  _putchar(sum_2);
 }
