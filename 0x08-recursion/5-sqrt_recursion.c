88661649ae9cf4bee5b04158206e7d5d70af99f3
Ibidapo-chad <ibidapo.chad@gmail.com>
2021-02-25 20:56:58 +0000

    Ex-05 modified

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,22 @@
 #include "holberton.h"
 
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int low, int high)
 {
-  if ((n*n > n - t) and (n*n <= n + t)):
-    return x;
-  n = (n + x/n)/2;
-  return _sqrt_recursion(n);
+  int mid;
+    
+  mid = (high + low) / 2;
+  /* x(n+1) = 1/2( x(n) + a / x(n) ) */
+
+  /* Base Case */
+  if((mid*mid) == high)
+    return mid;
+
+  /* Stops when the high and low numbers are within 0.001 of one another. */
+  if((high - low) < 0.001)
+    return mid;
+    
+  if((mid*mid) > high)
+    /* return square_root(low, mid) */
+        
+    return square_root(mid, high);
 }
