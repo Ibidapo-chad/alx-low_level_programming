4ced91c83e46356f3a761b7fa023809dcfbf040d
Ibidapo-chad <ibidapo.chad@gmail.com>
2021-03-03 20:17:15 +0000

    adv-task-06 present and modified task-05

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,11 +1,10 @@
 #include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <limits.h>
 
 
 /**
- *  free_grid - prints name of its file.
+ *  argstostr - prints name of its file.
  *  @ac: argument count
  *  @av: argument vector/string input
  *
@@ -13,42 +12,31 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i;
-	char **arg;
-	char *s;
+	int i, j, k, len;
+	char *arg;
 
-	arg = av;
-	arg = malloc(sizeof(char) * ac);
-	if (ac == 0 || arg == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
+	len = 0;
 	for (i = 0; i < ac; i++)
 	{
-		_puts(arg[i]);
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++;
 	}
-	;
-	return (s);
-}
-
-/**
- *  _puts - program prints string to screen
- *  @s: string array / string literal
- *
- *  Return: break if null or newline if whitespace checks true.
- */
-void _puts(char *s)
-{
-	int i;
-
-		if (*s)
+	arg = malloc(sizeof(char) * (len + 1));
+	if (arg == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			for (i = 0; ; i++)
-			{
-				if (_putchar(s[i]) == '\0')
-					break;
-				if (_putchar(s[i]) == ' ')
-					_putchar('\n');
-				_putchar(s[i]);
-			}
+			arg[k] = av[i][j];
+			k++;
 		}
-
+		arg[k] = '\n';
+		k++;
+	}
+	return (arg);
 }
