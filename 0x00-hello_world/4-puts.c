316e3296762cc107900a2a7211ec9506005088c7
Ibidapo-chad <Ibidapo.chad@gmail.com>
2021-03-18 02:47:12 +0000

    fix: task 00 corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - entry point
- * Return - Ends program
+ * main - Entry point
+ * @void: no arguments
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  int ch;
-  ch = "Programming is like building a multilingual puzzle\n";
-  int puts(ch);
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
